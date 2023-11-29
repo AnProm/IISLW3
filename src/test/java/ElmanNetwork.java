d94d5170b6af3de31e6c883e5c18df797c4433bd
AnProm <styralpalll@mail.ru>
2023-11-29 05:40:08 +0400

    Graphic added

--- src/test/java/ElmanNetwork.java
+++ src/test/java/ElmanNetwork.java
@@ -1,6 +1,15 @@
 import Dataset.DatasetUtil;
 import Dataset.IrisEntity;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.DefaultXYDataset;
+
+import javax.swing.*;
+import java.awt.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -216,5 +225,107 @@ public class ElmanNetwork {
         int epochs = 100;
 
         elmanNet.train(irisData, lambda, learningRate, momentum, epochs);
+
+        // Пример использования для построения графиков
+        inputSize = 4;
+        outputSize = 3;
+        lambda = 0.001;
+        momentum = 0.9;
+        epochs = 100;
+
+// Значения числа нейронов скрытого слоя для анализа зависимости от погрешности обучения
+        int[] hiddenLayerSizes = {4, 8, 12, 16};
+        int[] epochsValues = {50, 100, 150, 200};
+
+        DefaultXYDataset errorDataset = new DefaultXYDataset();
+
+        for (int hiddenSize1 : hiddenLayerSizes) {
+            double[][] data = new double[2][epochsValues.length];
+            for (int i = 0; i < epochsValues.length; i++) {
+                ElmanNetwork elmanNet1 = new ElmanNetwork(inputSize, hiddenSize1, outputSize);
+                double[] errors = trainAndGetErrors(elmanNet1, irisData, lambda, learningRate, momentum, epochsValues[i]);
+                data[0][i] = epochsValues[i];
+                data[1][i] = errors[epochsValues[i] - 1]; // берем ошибку на последней эпохе
+            }
+            errorDataset.addSeries("Hidden Layer Size: " + hiddenSize1, data);
+        }
+
+        JFreeChart errorChart = createChart("Error vs Epochs", "Epochs", "Error", errorDataset);
+
+        displayChart(errorChart);
+    }
+
+    private static double[] trainAndGetErrors(ElmanNetwork elmanNet, List<IrisEntity> data,
+                                              double lambda, double learningRate, double momentum, int epochs) {
+        double[] errors = new double[epochs];
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            double totalLoss = 0;
+
+            for (IrisEntity item : data) {
+                double[] input = {item.sepalLengthCm, item.sepalWidthCm, item.petalLengthCm, item.petalWidthCm};
+                double[] target = convertToTarget(item.species);
+
+                elmanNet.backPropagationWithMomentum(input, target, lambda, learningRate, momentum);
+
+                double[] output = elmanNet.feedForward(input);
+                totalLoss += elmanNet.calculateLoss(output, target);
+            }
+
+            errors[epoch] = totalLoss / data.size();
+        }
+        return errors;
+    }
+
+
+    private static double[] convertToTarget(int species) {
+        // Метод для преобразования значения класса в вектор целей
+        double[] target = new double[3];
+        target[species] = 1;
+        return target;
+    }
+
+    private static int getMaxIndex(double[] array) {
+        int maxIndex = 0;
+        double max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    private static void addDataToDataset(DefaultXYDataset dataset, String seriesName, int[] xData, double[] yData) {
+        double[][] data = new double[2][xData.length];
+        for (int i = 0; i < xData.length; i++) {
+            data[0][i] = xData[i];
+            data[1][i] = yData[i];
+        }
+        dataset.addSeries(seriesName, data);
+    }
+
+    private static JFreeChart createChart(String title, String xAxisLabel, String yAxisLabel, DefaultXYDataset dataset) {
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                title,
+                xAxisLabel,
+                yAxisLabel,
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+        return chart;
+    }
+
+    private static void displayChart(JFreeChart chart) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame("Chart");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(new ChartPanel(chart));
+            frame.pack();
+            frame.setVisible(true);
+        });
     }
 }
